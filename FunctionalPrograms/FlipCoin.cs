40d9838eee08a7a7ebc0b030d6fc25bf0e2cc542
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-13 18:19:33 +0530

    [Vaibhav] Add:Flip Coin

---
+++ FunctionalPrograms/FlipCoin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class FlipCoin
+    {
+        public void Flip(int noOfTimes)
+        {
+            int head = 0;
+            int tail = 0;
+            int headcount = 0; 
+            int tailcount = 0;
+
+            if (noOfTimes > 0)
+            {
+                for (int i = 0; i < noOfTimes; i++)
+                {
+                    Random random = new Random();
+                    int coin = random.Next(2);
+
+                    if (coin == 0)
+                    {
+                        Console.WriteLine("Its Head!");
+                        head = 1;
+                        headcount += 1;
+                    }
+                    else if (coin == 1)
+                    {
+                        Console.WriteLine("Its Tail!");
+                        tail = 1;
+                        tailcount += 1;
+                    }
+                }
+            }
+            double headPercentage = (headcount * 100) / noOfTimes;
+            double tailPercentage = (tailcount * 100) / noOfTimes;
+            Console.WriteLine($"Head Percentage ----> {headPercentage} \n Tail Percentage ----> {tailPercentage}");
+        }
+        /*const double VALIDATION_CHECK = 0.5;
+        public void Flip(int numOfTimes)
+        {
+            int head = 0, tail = 0;
+            Random random = new Random();
+            for (int i = 0; i < numOfTimes; i++)
+            {
+                double value = random.NextDouble();
+                if (value < VALIDATION_CHECK)
+                    tail++;
+                else
+                    head++;
+            }
+            double headPercentage = (head / numOfTimes) * 100;
+            double tailPercentage = (tail / numOfTimes) * 100;
+            Console.WriteLine("Head Percentage ------> " + headPercentage + "\n Tail Percentage ------> " + tailPercentage);
+        }*/
+    }
+}
---
+++ FunctionalPrograms/Program.cs
@@ -0,0 +1,42 @@
+﻿using FunctionalPrograms;
+using System;
+
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional Programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose an option to execute \n 1. Flip Coin"); //\n 2.Power of 2 \n 3. Prime Factor \n 4.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1: FlipCoin flip = new FlipCoin();
+                        Console.WriteLine("Enter num of flips: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        flip.Flip(num);
+                        break;
+                    /*case 2: 
+                        PowerOfTwo power = new PowerOfTwo();
+                        Console.WriteLine("Enter the power: ");
+                        int num1 = Convert.ToByte(Console.ReadLine());
+                        power.Display(num1);
+                        break;
+                    case 3:
+                        PrimeFactor prime = new PrimeFactor();
+                        Console.WriteLine("Enter the number: ");
+                        int num2 = Convert.ToInt32(Console.ReadLine());
+                        prime.factor(num2);
+                        break;*/
+                    case 4:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
